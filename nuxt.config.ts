1168274e51cc1df10195bc17304f708112679c82
Eban <contact@eban.eu.org>
2025-02-06 21:02:20 +0100

    feat: add PWA support by including Nuxt PWA Manifest and updating dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxtjs/tailwindcss', 'shadcn-nuxt', "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxtjs/tailwindcss', 'shadcn-nuxt', "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", "@vite-pwa/nuxt"],
   css: ['~/assets/css/main.css', 'assets/css/tailwind.css'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
